0180b6a9c3dcada2636f406eec1509539efbcdde
arck86 <j_checa_rodriguez@hotmail.com>
2022-01-19 22:24:44 +0100

    navigation and rooting basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list-component'
 import { ConvertToSpacesPipe } from './CustomerPipe/convertToSpacePipe';
 import { StarComponent } from './product/star/star.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
+    //,PageNoFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+     // {path: '**', component: PageNoFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
